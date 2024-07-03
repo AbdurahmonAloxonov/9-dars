a493fa2685bdfae74dcbce95f07e3a693bf9ce2f
Abdurahmon <abdurahmonalohanov671@gmail.com>
2024-07-03 15:10:46 +0500

    todo app

---
+++ app.js
@@ -0,0 +1,25 @@
+// let isExistbook = false;``
+
+// let promise = new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//         if (isExistbook) {
+//             resolve ("Kitob masvjud")
+//         } else {
+//             reject("kitob dokon yopiq ekan")
+//         }
+//       },2000)
+// })
+
+
+
+// promise
+
+//     .then
+
+
+
+async function fetchdata() {
+    return "data fetched";
+}
+
+fetchdata().then(data => console.log(data))
